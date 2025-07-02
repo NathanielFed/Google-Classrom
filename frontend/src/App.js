83c30c33ff634efcabd807d8f16f6fb05e0f1b46
carmeeb <200177824+carmeeb@users.noreply.github.com>
2025-07-02 21:06:53 +0800

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import Login from './Pages/Login';
-import Register from './Pages/Register';
-import Stream from './Components/Stream';
-import CLassForm from './Components/ClassForm';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/classForm" element={<CLassForm />} />
-      <Route path="/stream" element={<Stream />} />
-    </Routes>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
 export default App;
-
