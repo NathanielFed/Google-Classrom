5e10288a9c2f9fae1269103df473c877a437b1c1
eric <ebituin@gbox.adnu.edu.ph>
2025-07-03 08:48:30 +0800

    clyde

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Login from './Pages/Login';
 import Register from './Pages/Register';
 import CLassForm from './Components/ClassForm';
+import CreateAssignmentForm from './Components/CreateAssignmentForm';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/classForm" element={<CLassForm />} />
+      <Route path="/createAssignmentForm" element={<CreateAssignmentForm />} />
     </Routes>
   );
 }
