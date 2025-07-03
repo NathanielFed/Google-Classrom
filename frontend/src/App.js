9a2465cf75020f64309c046e566a67ffbf3856c1
Karie <dmmagalona@gbox.adnu.edu.ph>
2025-07-03 23:03:40 +0800

    message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,17 +15,19 @@ import CreateAssignmentForm from './Components/CreateAssignmentForm';
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/classForm" element={<CLassForm />} />
-        <Route path="/stream" element={<Stream />} />
-        <Route path="/gradingForm" element={<GradingForm />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/login" element={<Login />} />
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/classForm" element={<CLassForm />} />
+      <Route path="/stream" element={<Stream />} />
+      <Route path="/gradingForm" element={<GradingForm />} />
+      <Route path="/dashboard" element={<Dashboard/>}/>
+       <Route path="/login" element={<Login />} />
         <Route path="/createAssignmentForm" element={<CreateAssignmentForm />} />
-        {/* Add more routes as needed */}
-      </Routes>
+     
+      {/* Add more routes as needed */}
+    </Routes>
     </BrowserRouter>
   );
 }
