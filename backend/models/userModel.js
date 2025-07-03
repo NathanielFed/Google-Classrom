e1e562378d50faef82c6e7017af81e5b0114aa61
Clyde Dranreb B. Arizala <cdarizala@gbox.adnu.edu.ph>
2025-07-03 09:41:47 +0800

    aaa

---
+++ backend/models/userModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  accessToken: {
+    type: String
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('User', userSchema);
